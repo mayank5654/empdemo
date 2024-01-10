d3315c34e251f579144cbc5893f6256390ddcc5f
mayank5654 <mayankkaushal99@gmail.com>
2024-01-10 10:30:44 +0530

    intial commit

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppemp
+{
+    internal class Employee
+    {
+       
+            public int id { get; set; }
+            public string? name { get; set; }
+            public string? designation { get; set; }
+            public DateTime doj { get; set; }
+        
+    }
+}
